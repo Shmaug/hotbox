b2a85f9a7bbd8db3f1c150fca37ad1e478a7459f
Trevor Hedstrom <trevorhedstrom@gmail.com>
2015-05-22 00:09:26 -0700

    cant test now, maybe later

--- arduino/motorcontrol/motorcontrol.ino
+++ arduino/motorcontrol/motorcontrol.ino
@@ -1,10 +1,10 @@
-#define m1 9;
-#define m2 10;
-#define minThro=136; // full reverse
-#define midThro 191; // neutral
-#define maxThro 243; // full forward
-#define revDif 55; // mid - min
-#define forDif 52; // max - mid
+#define m1 9
+#define m2 10
+#define minThro 136 // full reverse
+#define midThro 191 // neutral
+#define maxThro 243 // full forward
+#define revDif 55 // mid - min
+#define forDif 52 // max - mid
 int cM;
 int lastDir;
 
@@ -23,12 +23,20 @@ void f(int thr){ // go forward at (thr)% throttle
   float f=(float)thr/100;
   analogWrite(cM,midThro-(forDif*f));
 }
-void s(int del){ // brake and stop
-  
+void n(){
+  analogWrite(cM,midThro);
 }
-
-void w(int v){
-  
+void s(){ // brake and stop
+  if (lastDir > 0){
+    b(50);
+    delay(500);
+    n();
+    
+  }else{
+    f(50);
+    delay(500);
+    n();
+  }
 }
 
 void setup() {
@@ -42,5 +50,19 @@ void setup() {
 }
 
 void loop() {
+  digitalWrite(13,HIGH);
+  f(75);
+  delay(1000);
+  digitalWrite(13,LOW);
   
+  s();
+  delay(1000);
+  
+  digitalWrite(13,HIGH);
+  b(75);
+  delay(1000);
+  digitalWrite(13,LOW);
+  
+  s();
+  delay(1000);
 }
